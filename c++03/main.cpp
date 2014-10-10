f9f5c9eaf5a404df34a349d788ac8bf61c60c13e
Liu Xiwang <1574721517@qq.com>
2014-10-10 15:58:15 +0800

    Initial Commit

---
+++ c++03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++03
+//
+//  Created by user on 14-10-10.
+//  Copyright (c) 2014年 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
