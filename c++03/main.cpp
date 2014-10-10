364add2a33a3b82bb575b57018b8d7df162b956a
Liu Xiwang <1574721517@qq.com>
2014-10-10 18:04:22 +0800

    1qa
    1qa

--- c++03/main.cpp
+++ c++03/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
-
+#include <iomanip>
+#include <cmath>
+//#include <math.h>
+#define PRICE 30//符号常量
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+//    int num,total;
+//    num=10;
+//    total=num*PRICE;
+//    std::cout << "total="<<total<<endl;
+    //强制类型转换
+//    float x;
+//    int i;
+//    x=3.6;
+//    i=(int)x;
+//    cout<<x<<"     "<<i<<endl;
+//    unsigned short a;
+//    short int b=-1;
+//    a=b;
+//    cout<<"a="<<a<<endl;
+    
+    //控制符
+    double a=123.456789012345;
+   // cout<<a;
+    cout<<setprecision(9)<<a;
     return 0;
 }
